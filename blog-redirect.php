e7d95e413d3c020200ab4b8c80875a71be8943d0
Deepak Parmar <deepakparmaronline@gmail.com>
2026-09-17 20:05:09 +0530

    Fix article legacy redirect category mapping

--- blog-redirect.php
+++ blog-redirect.php
@@ -6,6 +6,6 @@ if (!$post) {
     header('Location: /chatgpt/', true, 301);
     exit;
 }
-$category = post_category_key($post['title'].' '.$post['slug'].' '.$post['category']);
+$category = post_category_key($post['category']);
 header('Location: /'.$category.'/'.$slug, true, 301);
 exit;
