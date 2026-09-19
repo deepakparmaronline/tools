b054fdbf7eacb3e47964c9f4e52fac959e7110d1
Deepak Parmar <deepakparmaronline@gmail.com>
2026-09-19 20:23:22 +0530

    Remove temporary youth safety route exception

--- category-article.php
+++ category-article.php
@@ -15,9 +15,6 @@ if (!$post || post_category_key($post['category']) !== $category) {
     exit;
 }
 $source = __DIR__.'/blog/'.$slug.'.php';
-if ($slug === 'australian-youth-safety-blueprint-six-pillars') {
-    $source = __DIR__.'/blog/australian-youth-safety-runtime.php';
-}
 if (!is_file($source)) {
     http_response_code(404);
     include __DIR__.'/404.php';
